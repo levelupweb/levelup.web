6746c2c27d93caceb4e1fb3279cc03520a8f947c
kv9991 <exide-pra@yandex.ru>
2017-08-27 16:02:06 +0300

    config token

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ export default new function() {
 	this.type = this.ssl ? "https://" : "http://";
 	this.domain = this.type + "web.levelupworlds.com";
 	this.staticURL = this.domain + this.staticFolder;
-	this.host = "185.22.232.114";
+	this.host = this.dev ? "185.22.232.114" : "127.0.0.1";
 	this.hosturl = this.type + this.host;
-	this.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJ2aWNlSWQiOiIwMzI1ODkzNCIsImlhdCI6MTUwMzgzNTAwNH0.re9GGadfB3v3VNfctfYUsyHvZ0oDHXFQHHoeX4YrrxM"
+	this.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3ZWIubGV2ZWx1cHdvcmxkcy5jb20iLCJpYXQiOjE1MDM4Mzc2OTUsImV4cCI6MTUzNTM3MzcwMywiYXVkIjoiMTg1LjIyLjIzMi4xMTQiLCJzdWIiOiIwMzI1ODkzNCIsImFwcGxpY2F0aW9uX2lkIjoiMDMyNTg5MzQifQ.s5-dmQTAUlavJfofRUM9PhXFIHbW0D9WK4nipFfAFT4"
 	this.mail = new function() {
 		this.port = "3080";
 		this.sendURL = config.hosturl + ':' + this.port + '/send';
