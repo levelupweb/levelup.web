46789237ed9fcc757bb928e5ecab61f2d4270f30
kv9991 <exide-pra@yandex.ru>
2017-08-31 01:18:45 +0300

    edit config + new burger

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ export default new function() {
 	this.type = this.ssl ? "https://" : "http://";
 	this.domain = this.type + "web.levelupworlds.com";
 	this.staticURL = this.domain + this.staticFolder;
-	this.host = this.dev ? "185.22.232.114" : "127.0.0.1";
+	this.host = !this.dev ? "185.22.232.114" : "127.0.0.1";
 	this.hosturl = this.type + this.host;
 	this.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3ZWIubGV2ZWx1cHdvcmxkcy5jb20iLCJpYXQiOjE1MDM4Mzc2OTUsImV4cCI6MTUzNTM3MzcwMywiYXVkIjoiMTg1LjIyLjIzMi4xMTQiLCJzdWIiOiIwMzI1ODkzNCIsImFwcGxpY2F0aW9uX2lkIjoiMDMyNTg5MzQifQ.s5-dmQTAUlavJfofRUM9PhXFIHbW0D9WK4nipFfAFT4"
 	this.mail = new function() {
